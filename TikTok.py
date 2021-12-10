f78b7a48c8eb70ee196223f186323e33e4066f17
DARTH <95735573+0xDARTH@users.noreply.github.com>
2021-12-10 18:56:30 +0300

    Add files via upload

---
+++ TikTok.py
@@ -0,0 +1,78 @@
+import requests
+import random
+from colorama import *
+g = Fore.GREEN
+r = Fore.RED
+y = Fore.YELLOW
+c = Fore.CYAN
+print(r + '''
+
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢤⣶⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⡾⠿⢿⡀⠀⠀⠀⠀⣠⣶⣿⣷⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣦⣴⣿⡋⠀⠀⠈⢳⡄⠀⢠⣾⣿⠁⠈⣿⡆⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⣰⣿⣿⠿⠛⠉⠉⠁⠀⠀⠀⠹⡄⣿⣿⣿⠀⠀⢹⡇⠀⠀⠀
+⠀⠀⠀⠀⠀⣠⣾⡿⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⣰⣏⢻⣿⣿⡆⠀⠸⣿⠀⠀⠀
+⠀⠀⠀⢀⣴⠟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣾⣿⣿⣆⠹⣿⣷⠀⢘⣿⠀⠀⠀
+⠀⠀⢀⡾⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⣿⣿⠋⠉⠛⠂⠹⠿⣲⣿⣿⣧⠀⠀
+⠀⢠⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣤⣿⣿⣿⣷⣾⣿⡇⢀⠀⣼⣿⣿⣿⣧⠀
+⠰⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⡘⢿⣿⣿⣿⠀
+⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⣷⡈⠿⢿⣿⡆
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠛⠁⢙⠛⣿⣿⣿⣿⡟⠀⡿⠀⠀⢀⣿⡇
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣶⣤⣉⣛⠻⠇⢠⣿⣾⣿⡄⢻⡇
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⣦⣤⣾⣿⣿⣿⣿⣆⠁ ''')
+print(c+'================================================')
+print('==========INSTAGRAM@dqfa======SNAPCHAT:UBHC=====')
+print('====================GITHUB:0xDARTH==============')
+
+
+rt = requests.session()
+litters = 'qwertyuiopasdfghjklzxcvbnm1234567890._'
+
+us = input("[1] User length from 4\n[2] User length from 3\n====>")
+
+id = input("[+] Enter Id : ")
+
+token = input("[+] Enter Bot Token : ")
+print('')
+print('================================================')
+
+hea = {
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'accept-encoding': 'gzip, deflate, br',
+        'accept-language': 'en-US,en;q=0.9,ar;q=0.8',
+        'cache-control': 'max-age=0',
+        'sec-ch-ua': '" Not;A Brand";v="99", "Google Chrome";v="91", "Chromium";v="91"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-fetch-dest': 'document',
+        'sec-fetch-mode': 'navigate',
+        'sec-fetch-site': 'same-origin',
+        'sec-fetch-user': '?1',
+        'upgrade-insecure-requests': '1',
+        'user-agent': 'Mozilla/5.0(Windows NT 10.0;Win64;x64) AppleWebKit/537.36(KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+    }
+if us == "1":
+    while True:
+        user = str("".join(random.choice(litters)for x in range(0)))
+        user1 = str("".join(random.choice(litters)for x in range(4)))
+        usernames = user + user1
+        tiko = f'https://www.tiktok.com/@{usernames}?'
+        reqsnd = rt.get(tiko, headers=hea).status_code
+        if reqsnd == 404:
+         print(g + f' [+] available : {usernames} ')
+         bot = f"https://api.telegram.org/bot{token}/sendMessage?chat_id={id}&text= available : {usernames}\n𝘽𝙔 : @dqfa"
+         rt.get(bot)
+        else:
+          print(r + f' [+] Unavailable : {usernames}  ')
+if us == "2":
+    while True:
+        user = str("".join(random.choice(litters)for x in range(0)))
+        user1 = str("".join(random.choice(litters)for x in range(3)))
+        usernames = user + user1
+        tiko = f'https://www.tiktok.com/@{usernames}?'
+        reqsnd = rt.get(tiko, headers=hea).status_code
+        if reqsnd == 404:
+         print(g + f' [+] available : {usernames} ')
+         bot = f"https://api.telegram.org/bot{token}/sendMessage?chat_id={id}&text= available : {usernames}\n𝘽𝙔 : @dqfa"
+         rt.get(bot)
+        else:
+          print(r + f' [+] Unavailable : {usernames}  ')
